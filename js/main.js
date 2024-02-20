b23030a8bff303ae9d04fbc8a05185fc4b6d80a8
SneatX <santi02005@hotmail.com>
2024-02-20 16:48:37 -0500

    feat: :sparkles: Funcionamiento de la navBar desplegable

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 import {} from "./module/facturacion.js"
+
+const menu = document.querySelector(".menu")
+const menuBtn = document.querySelector(".menu-btn")
+
+menuBtn.addEventListener("click" , (event)=>{
+    menu.classList.toggle("nav-toggle")
+} )
